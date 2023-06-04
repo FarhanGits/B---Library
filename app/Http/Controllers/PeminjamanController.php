fdd22d2b789493b318c4039ecf96ac03d185f493
FarhanGits <daffafarhan05@gmail.com>
2023-06-04 21:23:34 +0700

    finishing pengembalian

--- app/Http/Controllers/PeminjamanController.php
+++ app/Http/Controllers/PeminjamanController.php
@@ -12,11 +12,30 @@ use function GuzzleHttp\Promise\all;
 
 class PeminjamanController extends Controller
 {
-    public function pinjamBuku(Request $request, DfBuku $dfbuku){
+    public function pinjamBuku(Request $request){
         $rentDate = Carbon::create($request['tanggalPinjam']);
         $request['tanggalKembali'] = $rentDate->addDays(5);
         // dd($request->all());
         Peminjaman::create($request->all());
         return redirect('/daftarkoleksi-user');
+    }
+
+    public function viewPengembalian() {
+        $data = Peminjaman::paginate(20);
+        return view('Admin/dashboard-pengembalian-buku', compact('data'));
+    }
+
+    public function logKonfirmasi($id) {
+        $data = Peminjaman::with(['user', 'book'])->find($id);
+        return view('Admin/dashboard-widget-pengembalian', compact('data'));
+    }
+
+    public function bukuKembali(Request $request, $id) {
+        $data = Peminjaman::with(['user', 'book'])->find($id);
+        $data['tanggalKembaliAsli'] = Carbon::now();
+        // dd($data->all());
+        $data->update($request->all());
+        $data->save();
+        return redirect('/pengembalianbuku');
     }
 }
